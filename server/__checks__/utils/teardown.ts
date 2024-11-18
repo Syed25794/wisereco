3946799404f7640e6fb8862b81e09e972b713212
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2024-11-18 22:39:38 +0530

    fixed the error in of teardown script

--- server/__checks__/utils/teardown.ts
+++ server/__checks__/utils/teardown.ts
@@ -1,19 +1,15 @@
 import axios from 'axios';
 declare const response : any ;
 
-const getCreatedNoteId = async ()=> {
-    console.log('Here we are...');
-    const body = JSON.parse(response.body);
-    const { createdNote } = body;
-    const { _id } = createdNote;
-    
-    // setup the correct url and its parameters
-    const host = 'https://wisereco.onrender.com'
-    const path = `/notes/${_id}`
-    const url = host + path;
-    console.log('url',url);
-    const notes = await axios.delete(url);
-    console.log('notes',notes.data);
-}
+console.log('Here we are...');
+const body = JSON.parse(response.body);
+const { createdNote } = body;
+const { _id } = createdNote;
 
-getCreatedNoteId()
+// setup the correct url and its parameters
+const host = 'https://wisereco.onrender.com'
+const path = `/notes/${_id}`
+const url = host + path;
+console.log('url',url);
+const notes = await axios.delete(url);
+console.log('notes',notes.data);
